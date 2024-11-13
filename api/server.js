c567fe3147dcc9540a19dfa0bb1888b1e21adb8d
Amit <amit.yd725@gmail.com>
2024-11-14 02:56:51 +0530

    allowed cors('*')

--- api/server.js
+++ api/server.js
@@ -8,7 +8,13 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: '*', // Allows all origins; replace '*' with specific URLs to restrict
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  }));
+  
 
 // Import Routes
 const authRoutes = require('../routes/authRoutes');
